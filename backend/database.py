6fdc9949c7f359c8a9fd033c9d762376e567d6dc
Bibhu Bismay <bismaybibhu07@gmail.com>
2026-08-13 01:38:37 +0530

    Cloud ready backend

--- backend/database.py
+++ backend/database.py
@@ -1,22 +1,7 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-from sqlalchemy.engine import URL
-
-DATABASE_URL = URL.create(
-    drivername="postgresql+psycopg2",
-    username="postgres",
-    password="Bibhu@63718",
-    host="localhost",
-    port=5432,
-    database="iter_notes_hub"
-)
-
-engine = create_engine(DATABASE_URL)
-
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
-
+import os
+from sqlalchemy import create_engine 
+from sqlalchemy.orm import sessionmaker, declarative_base 
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL) 
+SessionLocal = sessionmaker( autocommit=False, autoflush=False, bind=engine ) 
 Base = declarative_base()
